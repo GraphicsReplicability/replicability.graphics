f3d9ce33a46f37800165c7f65fbedd38ef2001b3
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2020-04-29 20:39:41 +0200

    edits

--- scripts/generatePagesFromConsolidatedJSON.py
+++ scripts/generatePagesFromConsolidatedJSON.py
@@ -646,8 +646,11 @@ def write_index_step2(findex, data):
         replicability in Computer Graphics, by evaluating whether the
         code is available and whether it works properly.  As a proxy
         for this field we compiled, ran and analyzed ''' + str(data["cptHasCode"])+ ''' codes out of
-       ''' + str(data["cpt"]) + ''' papers from SIGGRAPH conferences (exhaustive for 2014, 2016 and 2018). The
-        analysis provided in this <a href="https://replicability.graphics/replicability.pdf"><b>publication</b></a> (PDF, see <a href="index.html#team">below</a>) shows a clear increase in the number of papers with
+       ''' + str(data["cpt"]) + ''' papers from SIGGRAPH conferences (exhaustive for 2014, 2016 and 2018). In the analysis described in</p>
+
+                        <p>Nicolas Bonneel, David Coeurjolly, Julie Digne, Nicolas Mellado, <a href="https://replicability.graphics/replicability.pdf"><i><b>Code Replicability in Computer Graphics</b></i>, ACM Trans. on Graphics (Proceedings of SIGGRAPH 2020), 39:4</a>,</p>
+
+        <p style="text-align:left">we shows a clear increase in the number of papers with
         available and operational research codes with a dependency on
         the subfields, and indicates a correlation between code
         replicability and citation count. </p>
@@ -762,7 +765,6 @@ def write_index_step2(findex, data):
                 <li> <a href="https://www.irit.fr/~Nicolas.Mellado/">Nicolas Mellado</a> (CNRS, IRIT, Toulouse, France)
                 </ul>
 
-                        <p>Main reference paper: <a href="https://replicability.graphics/replicability.pdf">Nicolas Bonneel, David Coeurjolly, Julie Digne, Nicolas Mellado, <i>Code Replicability in Computer Graphics</i>, Siggraph 2020 </a></p>
 
                 <img height="100px" src="images/logo_cnrs.png"/>   &nbsp;&nbsp;&nbsp;  <img height="100px" src="images/logo_liris.png"/> &nbsp;&nbsp;&nbsp; <img height="100px"  src="images/logo_irit.png"/>
 
