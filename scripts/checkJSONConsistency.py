f7de2fbb9a4403616d50e18e63f405cbb37c8b11
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2020-05-15 13:45:23 +0200

    Checks

--- scripts/checkJSONConsistency.py
+++ scripts/checkJSONConsistency.py
@@ -2,16 +2,25 @@ from __future__ import print_function
 import csv,os,sys, json,re,codecs,datetime
 from difflib import SequenceMatcher
 
+###########################
+
 def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
 
     
 def check(text):
  if text!="":
+   fout = open("error.log", "a")
+   errorDetected=True
    eprint(text)
-   os._exit(42)
+   fout.write(text+'\n')
 
 #######################################
+
+def checkVariant(variant):
+  if not("Variant name" in variant):
+    return '[Variant name] A variant name must be given'
+  return ''
 
 def checkYear(variant):
   if variant['Year'] < 2000 or variant['Year'] > 2048:
@@ -21,41 +30,41 @@ def checkYear(variant):
 def checkTopics(variant):
   listTopics = {"Rendering", "Animation and Simulation", "Geometry", "Images", "Virtual Reality", "Fabrication"}
   if not(variant['Topic {Rendering, Animation and Simulation, Geometry, Images, Virtual Reality, Fabrication}'] in listTopics):
-    return '[Topic check] The following topic is not in {Rendering, Animation and Simulation, Geometry, Images, Virtual Reality, Fabrication}: '+ str(variant['Topic {Rendering, Animation and Simulation, Geometry, Images, Virtual Reality, Fabrication}'])
+    return '[Topic check]{Variant: '+variant["Variant name"]+'} The following topic is not in {Rendering, Animation and Simulation, Geometry, Images, Virtual Reality, Fabrication}: '+ str(variant['Topic {Rendering, Animation and Simulation, Geometry, Images, Virtual Reality, Fabrication}'])
   return ""
  
 def checkScore(variant,score):
   if variant[score]== "":
     return ""
   if type(variant[score])==str:
-    return  '[Score check] the score '+variant[score]+' must be a number, not a string'
+    return  '[Score check]{Variant: '+variant["Variant name"]+'} the score '+variant[score]+' must be a number, not a string'
   if variant[score] < 0 or variant[score] > 5:
-    return '[Score check] problem with the following score: '+ score+', I read  '+str(variant[score])
+    return '[Score check]{Variant: '+variant["Variant name"]+'} problem with the following score: '+ score+', I read  '+str(variant[score])
   return ""
 
 def checkTime(variant,score):
  if variant[score] < 0 or variant[score] > 10:
-  return '[Time check] problem with the time (must be in [0,10]): '+str(variant[score])
+  return '[Time check]{Variant: '+variant["Variant name"]+'} problem with the time (must be in [0,10]): '+str(variant[score])
  return ""
 
 def checkCodeURL(variant):
   if variant["Code available (boolean)"] == True and variant["Code URL"]=="":
-   return '[Code URL check] If code is provided (boolean), an URL must be given'
+   return '[Code URL check]{Variant: '+variant["Variant name"]+'} If code is provided (boolean), an URL must be given'
   return ""
 
 def checkCodeType(variant):
   if variant["Code available (boolean)"] == True and variant["Software type {Code, Binary, Partial Code}"]=="":
-   return '[Code type check] If code is provided (boolean), a software type  must be given'
+   return '[Code type check]{Variant: '+variant["Variant name"]+'} If code is provided (boolean), a software type  must be given'
   return ""
 
 def checkCodeType(variant):
   if variant["Code available (boolean)"] == True and variant["Software type {Code, Binary, Partial Code}"]=="":
-   return '[Code type check] If code/software is provided (boolean), a type must be given'
+   return '[Code type check]{Variant: '+variant["Variant name"]+'} If code/software is provided (boolean), a type must be given'
   return ""
 
 def checkAffiliation(variant):
   if not(variant['Co-authors from academia (boolean)'] or variant['Co-authors from industry (boolean)']):
-        return '[Affiliation check] Both "Co-authors from academia (boolean)" and "Co-authors from industry (boolean)" cannot be false'
+        return '[Affiliation check]{Variant: '+variant["Variant name"]+'} Both "Co-authors from academia (boolean)" and "Co-authors from industry (boolean)" cannot be false'
   return ""
 
 
@@ -67,7 +76,7 @@ def testMaster(paper):
     if variant['Is master variant (boolean)'] == True:
       cpt = cpt + 1
 
-    
+    check(checkVariant(variant))
     check(checkYear(variant))
     check(checkTopics(variant))
     check(checkAffiliation(variant))
@@ -93,6 +102,16 @@ def checkJSON(paperJSON):
   paper = json.load(open(paperJSON))
   testMaster(paper)
 
+
+errorDetected=False
+fout=open("error.log", "w")
+fout.write("");
+fout.close()
+
 for i in range(1, len(sys.argv)):
   eprint(" ======= Checking the content of the JSON " + sys.argv[i])
   checkJSON(sys.argv[i])
+  
+  if errorDetected:
+    eprint("Error detected, check error.log file")
+    os._exit(42)
