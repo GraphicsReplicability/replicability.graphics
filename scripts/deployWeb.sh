1897b3786273774970097fd56b41d1cd87775c3a
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2020-06-03 20:22:59 +0200

    forcing the delete

--- scripts/deployWeb.sh
+++ scripts/deployWeb.sh
@@ -3,21 +3,22 @@ set -e
 
 ## $1 is the Altmetric API key
 
+
 echo "========= Init =========="
 sudo apt-get install -y graphicsmagick
 if [ -d tmp ]; then
-  echo "tmp folder already exists"
-else
-  mkdir tmp        
+    echo "tmp folder already exists, cleaning it"
+    rm  -rf tmp
 fi
 
 echo "========= Downloading the data =========="
-if [ ! -d tmp ]; then
-    if [ ! -d .git ]; then
-        rm -rf ./*
-    fi
-    git clone --depth 1 -b gh-pages https://github.com/GraphicsReplicability/replicability.graphics.git tmp
-fi
+git clone --depth 1 -b gh-pages https://github.com/GraphicsReplicability/replicability.graphics.git tmp
+
+## removing the .git in the clone
+cd tmp
+rm -rf .git/
+cd ..
+
 cp -R website-source/* tmp/
 
 echo "========= Concatenate the JSON =========="
